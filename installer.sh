5dfd4443a51b89fee0ab60387bd828561280e862
Dexter <anaydevale@yahoo.in>
2021-10-22 16:00:16 +0530

    my first commit

---
+++ installer.sh
@@ -0,0 +1,33 @@
+chmod +x vapt1.sh
+cp -r vapt1.sh /usr/local/sbin
+echo "rm -rf /usr/local/sbin/vapt1.sh && echo -e '\e[1;31m    
+
+                       (UNINSTALLATION COMPLETED)
+
+\e[0m' && rm uninstaller.sh " > uninstaller.sh 
+
+
+
+echo -e '\e[1;33m         
+                       (INSTALLATION COMPLETED)
+
+                                                             \e[1;32m
+
+  To execute the framework type anywhere in your terminal "vapt1.sh".  \e[0m 
+'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
---
+++ uninstaller.sh
@@ -0,0 +1,5 @@
+rm -rf /usr/local/sbin/vapt1.sh && echo -e '\e[1;31m    
+
+                       (UNINSTALLATION COMPLETED)
+
+\e[0m' && rm uninstaller.sh 
